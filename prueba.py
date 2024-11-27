f0853c262ed432fb88bd3e1d5db251f2379ae24d
Anthar97 <nimontanar@unal.edu.co>
2024-11-27 15:56:44 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,3 +6,18 @@ l_leche= corral/30
 while (dia<=7):
     print("La vaca produjo ",(Vacas*(l_leche*dia)), "el dia", dia)
     dia= dia+1
+
+a = int (input("Ingrese la cantidad de aves de la granja: "))
+
+gallinas = a / 3
+
+gallinas_3_dias = gallinas / 2
+gallinas_5_dias = gallinas / 2
+
+
+
+
+huevo = (gallinas_3_dias * (10))
+huevo_2 = (gallinas_5_dias * (6))
+
+print ("Al mes se ponen", huevo + huevo_2,"huevos")
