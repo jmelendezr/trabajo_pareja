8593c140ee5dfa4469c3064e01acb02a0be8b6c7
jmelendezr <jmelendezr@unal.edu.co>
2024-11-27 15:57:29 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
+##Punto1
 Vacas = int(input("Ingrese el numero de vacas que cria: "))
 corral= 40*30
 print("El tamaño del corral es de ",corral, "metros cuadrados")
@@ -6,18 +7,13 @@ l_leche= corral/30
 while (dia<=7):
     print("La vaca produjo ",(Vacas*(l_leche*dia)), "el dia", dia)
     dia= dia+1
-
+##Punto2
 a = int (input("Ingrese la cantidad de aves de la granja: "))
-
 gallinas = a / 3
 
 gallinas_3_dias = gallinas / 2
 gallinas_5_dias = gallinas / 2
 
-
-
-
 huevo = (gallinas_3_dias * (10))
 huevo_2 = (gallinas_5_dias * (6))
-
 print ("Al mes se ponen", huevo + huevo_2,"huevos")
